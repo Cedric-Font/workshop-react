0a5f4e1c480d2145b5bc08033d3625c96fb7b0a9
Cedric-Font <font.cedric38@gmail.com>
2023-10-16 17:14:14 +0200

    starts css

--- workshop-react/src/component/Recipe.jsx
+++ workshop-react/src/component/Recipe.jsx
@@ -1,12 +1,14 @@
+import styles from "./recipe.module.css"
+
 function Recipe (props) {
 
     return <>
-       <h1>{props.nameRecipe}</h1>
-       <img src={props.imgSrc} alt ={props.imgSrc}/>
-       <p>{props.cookingTime}</p>
-       <p>{props.ingredients}</p>
-       <p>{props.recipeText}</p>
-       <p>{props.recipeSource}</p>
+       <h1 className={styles.name} >{props.nameRecipe}</h1>
+       <img  className={styles.img} src={props.imgSrc} alt ={props.imgSrc}/>
+       <p className={styles.time} >{props.cookingTime}</p>
+       <p className={styles.ingredients} >{props.ingredients}</p>
+       <p className={styles.text} >{props.recipeText}</p>
+       <p className={styles.resource} >{props.recipeSource}</p>
     </>
 }
 
